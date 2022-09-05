521b519d7e40eadc0a85fe61646d7daff16d59cc
Saeid <48330269+saeidabedi97@users.noreply.github.com>
2022-09-05 23:20:20 +0200

    changed the evironment variables

--- client.tsx
+++ client.tsx
@@ -2,14 +2,14 @@ import { createClient } from "next-sanity";
 import imageUrlBuilder from "@sanity/image-url";
 import { SanityImageSource } from "@sanity/image-url/lib/types/types";
 
-// export const config = {
-//   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+export const config = {
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
 
-//   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
-//   apiVersion: "2021-10-21",
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+  apiVersion: "2021-10-21",
 
-//   useCdn: process.env.NODE_ENV == "production",
-// };
+  useCdn: process.env.NODE_ENV == "production",
+};
 
 export const sanityClient = createClient({
   projectId: "1ks8y5qd",
