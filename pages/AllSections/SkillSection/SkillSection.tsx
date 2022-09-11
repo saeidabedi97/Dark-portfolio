4baaaceff853426cdf4096069d3b088229d3b2da
Saeid <48330269+saeidabedi97@users.noreply.github.com>
2022-09-12 00:13:27 +0200

    added animations

--- pages/AllSections/SkillSection/SkillSection.tsx
+++ pages/AllSections/SkillSection/SkillSection.tsx
@@ -1,20 +1,40 @@
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 import {
   Heading2,
   PMedium,
 } from "../../../components/AtomicComponents/Text/Text";
 import s from "../SkillSection/SkillSection.module.scss";
 import SlideShow from "../../../components/AtomicComponents/ImageSlideShow/SlideShow";
-import { VerticalLine } from "../../../components/AtomicComponents/VerticalLine/VerticalLine";
-import { HorizontalLine } from "../../../components/AtomicComponents/HorizontalLine/HorizontalLine";
 import st from "../../../styles/section.module.scss";
+import { useEffect } from "react";
 
 export default function SkillSection() {
+  const boxVariant = {
+    visible: { opacity: 1, scale: 1 },
+    hidden: { opacity: 0, scale: 0 },
+  };
+  const control = useAnimation();
+  const [ref, inView] = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      control.start("visible");
+    }
+  }, [control, inView]);
   return (
-    <section className={st.section}>
+    <motion.section
+      className={st.section}
+      ref={ref}
+      variants={boxVariant}
+      initial="hidden"
+      animate={control}
+      transition={{ duration: 1 }}
+    >
       <div className={s.skill__title__section}>
         <Heading2 className={st.section__heading}>Skill & tools </Heading2>
       </div>
       <SlideShow />
-    </section>
+    </motion.section>
   );
 }
